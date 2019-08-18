052398afd5e5e29db4e3fdacd3779aeea8d75a51
Hezretaly <hezretaly@icloud.com>
2019-08-18 20:26:08 +0800

    initial commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_18_041357) do
+ActiveRecord::Schema.define(version: 2019_08_18_122327) do
 
   create_table "cars", force: :cascade do |t|
     t.string "make"
